16076284c9ef6c620d448c3b571d27dc32a6c263
MahmoudMatar <mahmoudmatar49@gmail.com>
2023-12-27 03:33:10 +0200

    handle

--- src/main/java/org/example/HibernateTest.java
+++ src/main/java/org/example/HibernateTest.java
@@ -17,6 +17,9 @@ public class HibernateTest {
         Employee employee=new Employee();
         employee.setName("Mahmoud Matar");
 
+        Employee employee2=new Employee();
+        employee2.setName("Ahmed Matar");
+
 //      Take an instance from Cv Entity and set initial values
         Address employeeAddress=new Address();
         employeeAddress.setCountry("Employee about");
@@ -29,6 +32,9 @@ public class HibernateTest {
         Manger manger=new Manger();
         manger.setName("Mr. Ahmed");
 
+        Manger manger2=new Manger();
+        manger2.setName("Mr. Mustafa");
+
 //      Set the employee cv to employee in One-To-One Relationship
 //      Set the employee department to employee in One-To-Many Relationship
 //      add the employee to employee deportment in Many-To-one Relationship
@@ -36,8 +42,9 @@ public class HibernateTest {
         employee.setEmployeeAddress(employeeAddress);
         employee.setDepartment(department);
         department.getEmployees().add(employee);
-        employee.setMangerList(List.of(manger));
-        manger.setEmployeeList(List.of(employee));
+        manger.getEmployeeList().add(employee);
+        manger2.getEmployeeList().add(employee);
+        manger2.getEmployeeList().add(employee2);
 
 
 //        Using the Hibernate Api
@@ -47,7 +54,9 @@ public class HibernateTest {
         session.beginTransaction();
         // save model objects
         session.save(employee);
+        session.save(employee2);
         session.save(manger);
+        session.save(manger2);
         session.save(employeeAddress);
         session.save(department);
         session.getTransaction().commit();
@@ -56,11 +65,13 @@ public class HibernateTest {
         // Retrieving Objects using session.get
         employee=null;
         manger=null;
+        manger2=null;
         session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
 
         employee = session.get(Employee.class, 1);
         manger = session.get(Manger.class, 1);
+        manger2 = session.get(Manger.class, 2);
 
         session.getTransaction().commit();
         session.close();
